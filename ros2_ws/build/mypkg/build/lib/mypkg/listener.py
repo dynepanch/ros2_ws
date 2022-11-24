974148465c608daf9351cca4dce7bd6542bf2c3f
KenInaba <jiandaoye293@gmail.com>
2022-11-24 12:08:29 +0900

    person

--- ros2_ws/build/mypkg/build/lib/mypkg/listener.py
+++ ros2_ws/build/mypkg/build/lib/mypkg/listener.py
@@ -1,13 +1,14 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 def cd(msg):
         global node
-        node.get_logger().info("Listen: %d" % msg.data)
+        node.get_logger().info("Listen: %s" % msg)
 
 rclpy.init()
 node = Node("listner")
-pub = node.create_subscription(Int16,"countup",cd,10)
+pub = node.create_subscription(Person,"person",cd,10)
+
 rclpy.spin(node)
 
