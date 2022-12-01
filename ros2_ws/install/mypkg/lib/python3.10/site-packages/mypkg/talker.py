ef382735f7a8625086697aa1e9456bd6a6858573
KenInaba <jiandaoye293@gmail.com>
2022-12-01 11:18:43 +0900

    talker_listner_lesson10

--- ros2_ws/install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
+++ ros2_ws/install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
@@ -1,18 +1,19 @@
+#!/user/bin/python3
+
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Person,"person",10)
+pub = node.create_publisher(Int16, "countup", 10)
 n=0
-def cb():
+def cd():
     global n
-    msg = Person()
-    msg.name = "稲葉健" 
-    msg.age = n
+    msg=Int16()
+    msg.data = n
     pub.publish(msg)
     n+=1
 
-node.create_timer(0.5 , cb)
+node.create_timer(0.5,cd)
 rclpy.spin(node)
--- ros2_ws/src/mypkg/mypkg/listener.py
+++ ros2_ws/src/mypkg/mypkg/listener.py
@@ -1,14 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
 def cd(msg):
         global node
-        node.get_logger().info("Listen: %s" % msg)
+        node.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listner")
-pub = node.create_subscription(Person,"person",cd,10)
-
+pub = node.create_subscription(Int16,"countup",cd,10)
 rclpy.spin(node)
 
